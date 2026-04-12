cd5a42f0391c766b8353a4a3b20891b68818b6cb
Park, Su Eun <park_sueun@naver.com>
2026-04-12 12:33:50 +0800

    feat: Token Exception 클래스 생성(Invalid, Expired)

---
+++ src/main/java/com/aigo/speech/auth/exception/InvalidTokenException.java
@@ -0,0 +1,8 @@
+package com.aigo.speech.auth.exception;
+
+public class InvalidTokenException extends RuntimeException {
+
+    public InvalidTokenException(String message) {
+        super(message);
+    }
+}

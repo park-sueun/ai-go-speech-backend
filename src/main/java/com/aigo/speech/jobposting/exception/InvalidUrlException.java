422a6d2964053f187e314aad3e9f8e21512efdaf
Park, Su Eun <park_sueun@naver.com>
2026-04-18 20:59:53 +0800

    feat: 채용공고 파싱 및 분석 API Exception 정의
    # Conflicts:
    #	src/main/java/com/aigo/speech/global/exception/GlobalExceptionHandler.java

---
+++ src/main/java/com/aigo/speech/jobposting/exception/InvalidUrlException.java
@@ -0,0 +1,8 @@
+package com.aigo.speech.jobposting.exception;
+
+public class InvalidUrlException extends RuntimeException {
+
+	public InvalidUrlException (String message) {
+		super(message);
+	}
+}

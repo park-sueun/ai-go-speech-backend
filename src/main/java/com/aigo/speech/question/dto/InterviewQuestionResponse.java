e1c307f86633f927e93fdc271cb2d49f2b08086b
Park, Su Eun <park_sueun@naver.com>
2026-04-30 15:52:13 +0900

    fix: InterviewQuestionResponse, InterviewSessionResponse dto 변경

--- src/main/java/com/aigo/speech/question/dto/InterviewQuestionResponse.java
+++ src/main/java/com/aigo/speech/question/dto/InterviewQuestionResponse.java
@@ -2,8 +2,18 @@ package com.aigo.speech.question.dto;
 
 import java.util.UUID;
 
+import com.aigo.speech.question.entity.InterviewQuestion;
+
 public record InterviewQuestionResponse(
 	UUID uuid,
 	int sequenceOrder,
 	String content
-) {}
+) {
+	public static InterviewQuestionResponse from (InterviewQuestion question) {
+		return new InterviewQuestionResponse(
+			question.getUuid(),
+			question.getSequenceOrder(),
+			question.getContent()
+		);
+	}
+}
